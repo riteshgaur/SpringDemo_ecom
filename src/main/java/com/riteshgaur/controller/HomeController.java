089d46623514910112ee7ff2b2aa33ce0c76b9e5
Ritesh Gaur <gaur.ritesh@gmail.com>
2018-03-27 22:56:31 +0800

    Delete product along with Image

--- src/main/java/com/riteshgaur/controller/HomeController.java
+++ src/main/java/com/riteshgaur/controller/HomeController.java
@@ -13,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -118,7 +120,23 @@ public class HomeController {
 
 
     @RequestMapping("/admin/productInventory/deleteProduct/{id}")
-    public String deleteProduct(@PathVariable String id, Model model) {
+    public String deleteProduct(@PathVariable String id, Model model, HttpServletRequest request) {
+
+
+        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+        //path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\images\\" + product.getProductID() + ".png");
+        path = Paths.get(rootDirectory + "/WEB-INF/resources/images/" + id + ".png");
+
+
+        if (Files.exists(path)) {
+            try {
+                Files.delete(path);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
 
         productDao.deleteProduct(id);
         return "redirect:/admin/productInventory";
